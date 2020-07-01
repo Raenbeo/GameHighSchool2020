85a6af6d8a34da1286120cd9c7a078eb82e0b281
Jo won ho <akwdkk39@gmail.com>
2020-07-01 19:24:33 +0900

    닷지 폴더 추가함

---
+++ Dodge/Assets/Dodge/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        playerMove(speed);
+    }
+
+    public void playerMove(float speed)
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+        float fire1Axis = Input.GetAxis("Fire1");
+
+        if(fire1Axis >= 0.5)
+        {
+            Debug.Log("파이어엑시스");
+            Die();
+        }
+
+        rigidbody.AddForce(new Vector3(xAxis,0,yAxis) * speed);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            rigidbody.AddForce(Vector3.back * speed);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            rigidbody.AddForce(Vector3.forward * speed);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            rigidbody.AddForce(Vector3.left * speed);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            rigidbody.AddForce(Vector3.right * speed);
+        else if (Input.GetKeyDown(KeyCode.Space))
+            Die();
+    }
+
+    public void Die()
+    {
+        Debug.Log("뒈쥠");
+        gameObject.SetActive(false);
+    }
+}
---
+++ Dodge/Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Test : EditorWindow
+{
+   [MenuItem("Window/Test")]
+   static void init()
+    {
+        Test window = (Test)EditorWindow.GetWindow(typeof(Test));
+        window.Show();
+    }
+    void OnGUI()
+    {
+        Handles.color = Color.blue;
+        Handles.DrawRectangle(0,new Vector3(200,200),Quaternion.identity,100);
+        Handles.DrawSolidDisc(new Vector3(200, 200), Vector3.forward, 100); 
+        Handles.DrawSolidDisc(new Vector3(150, 150), Vector3.forward, 50); 
+        Handles.DrawSolidDisc(new Vector3(250, 150), Vector3.forward, 50); 
+    }
+}
