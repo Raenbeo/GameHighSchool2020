a832c9407216a0649415d0e07d54dd297bf9f53e
Jo won ho <akwdkk39@gmail.com>
2020-07-20 21:08:15 +0900

    슈팅게임 만들기 1일차

---
+++ ShootingGame/Assets/Scripts/BulletMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour
+{
+    public GameObject m_Bullet;
+
+    public float speed = 30;
+
+    public float destoryTime = 5;
+
+    float InGameTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InGameTime += Time.deltaTime;
+
+        if(destoryTime <= InGameTime)
+        {
+            GameObject.Destroy(m_Bullet);
+        }
+
+        Vector3 move = transform.up * speed * Time.deltaTime;
+        transform.position += move;
+    }
+}
---
+++ ShootingGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject m_Bullet;
+    public GameObject m_BulletSpanwPos;
+
+
+    public float speed = 30;
+
+    float inGameTime = 0;
+    public float setAttackTime = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inGameTime += Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) transform.position += Vector3.left * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow)) transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow)) transform.position += Vector3.down * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow)) transform.position += Vector3.up * speed * Time.deltaTime;
+
+        if (setAttackTime <= inGameTime)
+        {
+            Attack();
+            inGameTime = 0;
+        }
+    }
+    public void Attack()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject bullet = GameObject.Instantiate(m_Bullet);
+            bullet.transform.position = m_BulletSpanwPos.transform.position;
+        }
+    }
+}
+
+
+
