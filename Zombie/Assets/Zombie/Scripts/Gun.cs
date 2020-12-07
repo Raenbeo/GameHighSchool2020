901d1a8a1a93d88e711e922c318dc8adb20d9f5b
Jo won ho <akwdkk39@gmail.com>
2020-12-07 19:04:58 +0900

    End

---
+++ Zombie/Assets/Zombie/Scripts/Gun.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 총을 구현한다
+public class Gun : MonoBehaviour {
+    // 총의 상태를 표현하는데 사용할 타입을 선언한다
+    public enum State {
+        Ready, // 발사 준비됨
+        Empty, // 탄창이 빔
+        Reloading // 재장전 중
+    }
+
+    public State state { get; private set; } // 현재 총의 상태
+
+    public Transform fireTransform; // 총알이 발사될 위치
+
+    public ParticleSystem muzzleFlashEffect; // 총구 화염 효과
+    public ParticleSystem shellEjectEffect; // 탄피 배출 효과
+
+    private LineRenderer bulletLineRenderer; // 총알 궤적을 그리기 위한 렌더러
+
+    private AudioSource gunAudioPlayer; // 총 소리 재생기
+    public AudioClip shotClip; // 발사 소리
+    public AudioClip reloadClip; // 재장전 소리
+
+    public float damage = 25; // 공격력
+    private float fireDistance = 50f; // 사정거리
+
+    public int ammoRemain = 100; // 남은 전체 탄약
+    public int magCapacity = 25; // 탄창 용량
+    public int magAmmo; // 현재 탄창에 남아있는 탄약
+
+
+    public float timeBetFire = 0.12f; // 총알 발사 간격
+    public float reloadTime = 1.8f; // 재장전 소요 시간
+    private float lastFireTime; // 총을 마지막으로 발사한 시점
+
+
+    private void Awake() {
+        // 사용할 컴포넌트들의 참조를 가져오기
+        bulletLineRenderer = GetComponent<LineRenderer>();
+        gunAudioPlayer = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable() {
+        // 총 상태 초기화
+        //(수정...)탄창 크기에 맞춰 탄창에 총알을 가득채운다.
+        magAmmo = magCapacity;  
+    }
+
+    // 발사 시도
+    public void Fire() {
+        //마지막으로 총을 쏜시간 + 공격 딜레이 보다 현재 시간이 더 오래되었으면
+        if(lastFireTime + timeBetFire <= Time.time)
+        {
+            //(정담...) 탄창에 총알이 있다면
+            if(magAmmo > 0)
+            {
+                if(state == State.Ready)
+                    Shot();
+
+                //(수정...)총알을 쏠때마다 탄창의 총알은 1씩 소모
+                magAmmo -= 1;
+
+                //마지막으로 총을 쏜 시간은 현재.
+                lastFireTime = Time.time;
+            }
+        }
+    }
+
+    // 실제 발사 처리
+    private void Shot() 
+    {
+        RaycastHit hitInfo;
+        bool isHit = Physics.Raycast(fireTransform.position,
+            fireTransform.forward, 
+            out hitInfo,
+            fireDistance);
+
+        Vector3 hitPosition;
+        if (isHit)
+        {
+            hitPosition = hitInfo.point;
+
+            //수정(아래)
+            IDamageable[] damageables;
+            if (hitInfo.rigidbody != null)
+                damageables = hitInfo.rigidbody.GetComponents<IDamageable>();
+            else
+                damageables = hitInfo.collider.GetComponents<IDamageable>();
+
+            foreach (var damageable in damageables)
+            {
+                damageable.OnDamage(damage, hitPosition, hitInfo.normal);
+            }
+        }
+        else
+        {
+            hitPosition = fireTransform.position 
+                + fireTransform.forward * fireDistance;
+        }
+
+        StartCoroutine(ShotEffect(hitPosition));
+    }
+
+    // 발사 이펙트와 소리를 재생하고 총알 궤적을 그린다
+    private IEnumerator ShotEffect(Vector3 hitPosition) 
+    {
+        //총구에서 화염과 슈팅 소리가 난다.
+        muzzleFlashEffect.Play();
+        gunAudioPlayer.clip = shotClip;
+        gunAudioPlayer.Play();
+
+        //총알 궤적에 대한 이펙트가 정 위치에 보입니다.
+        List<Vector3> linePoint = new List<Vector3>();
+        linePoint.Add(fireTransform.position);
+        linePoint.Add(hitPosition);
+
+        bulletLineRenderer.SetPositions(linePoint.ToArray());
+
+
+        // 라인 렌더러를 활성화하여 총알 궤적을 그린다
+        bulletLineRenderer.enabled = true;
+
+        // 0.03초 동안 잠시 처리를 대기
+        yield return new WaitForSeconds(0.03f);
+
+        //탄피가 배출된다.
+        shellEjectEffect.Play();
+
+        // 라인 렌더러를 비활성화하여 총알 궤적을 지운다
+        bulletLineRenderer.enabled = false;
+    }
+
+    // 재장전 시도
+    public bool Reload() 
+    {
+        if (magAmmo >= magCapacity)
+        {
+            return false;
+        }
+        else if (ammoRemain <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            StartCoroutine(ReloadRoutine());
+
+            return true;
+        }
+    }
+
+    // 실제 재장전 처리를 진행
+    private IEnumerator ReloadRoutine() {
+        // 현재 상태를 재장전 중 상태로 전환
+        state = State.Reloading;
+        
+        // 재장전 소요 시간 만큼 처리를 쉬기
+        yield return new WaitForSeconds(reloadTime);
+
+        int requiredAmmo = magCapacity - magAmmo;
+
+        if (ammoRemain >= requiredAmmo)
+        {
+            magAmmo = magCapacity;
+            ammoRemain -= requiredAmmo;
+        }
+        else
+        {
+            magAmmo = ammoRemain;
+            ammoRemain = 0;
+        }
+
+        // 총의 현재 상태를 발사 준비된 상태로 변경
+        state = State.Ready;
+    }
+
+    private void Update()
+    {
+        /*추가*/ UIManager.instance.UpdateAmmoText(magAmmo, ammoRemain);
+    }
+}
---
+++ Zombie/Assets/Zombie/Scripts/PlayerMovement.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+// 플레이어 캐릭터를 사용자 입력에 따라 움직이는 스크립트
+public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 5f; // 앞뒤 움직임의 속도
+    public float rotateSpeed = 180f; // 좌우 회전 속도
+
+
+    private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
+    private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
+    private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
+
+    private void Start() {
+        // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
+    private void FixedUpdate() {
+        // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+
+        Move();
+
+        Rotate();
+
+        playerAnimator.SetFloat("Move", playerInput.move + playerInput.rotate);
+    }
+
+    // 입력값에 따라 캐릭터를 앞뒤로 움직임
+    private void Move() {
+        var forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        var right = Camera.main.transform.right;
+        right.y = 0;
+        right.Normalize();
+
+        var rigidboty = GetComponent<Rigidbody>();
+
+        Vector3 v = new Vector3();
+
+        v += forward
+            * moveSpeed
+            * playerInput.move;
+
+        v += right
+            * moveSpeed
+            * playerInput.rotate;
+
+        rigidboty.velocity = v;
+        return;
+        //hack;
+        //플레이어는 앞뒤로 움직인다.
+        transform.position += forward
+            * moveSpeed * Time.fixedDeltaTime
+            * playerInput.move;
+
+        //플레이어는 좌우로 움직인다.
+        transform.position += right
+            * moveSpeed * Time.fixedDeltaTime
+            * playerInput.rotate;
+
+
+
+        return;
+        //hack;
+        {
+            //앞뒤 움직임
+            transform.position
+                += transform.forward
+                   * moveSpeed * Time.fixedDeltaTime
+                   * playerInput.move;
+        }
+    }
+
+    // 입력값에 따라 캐릭터를 좌우로 회전
+    private void Rotate() {
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, 0);
+        float enter;
+        plane.Raycast(ray, out enter);
+        var point = ray.GetPoint(enter);
+        transform.LookAt(point);
+        var eulerAngles = transform.eulerAngles;
+        eulerAngles.x = 0;
+        eulerAngles.z = 0;
+        transform.eulerAngles = eulerAngles;
+
+        return;
+        //hack;
+        //좌 회전
+        transform.Rotate(0,
+            rotateSpeed * Time.fixedDeltaTime * playerInput.rotate,
+            0);
+    }
+}
